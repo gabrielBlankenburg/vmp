52955d383574325d98d0f336eb8d74fd17294db8
Udimberto Silvestre Junior <udi@ingresse.com>
2018-12-01 05:14:04 -0200

    Updates

---
+++ precache-manifest.b2e8c5075b3bf0eeabfffd27a15748fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fe7ee23094599ab062f",
+    "url": "/vmp/static/js/runtime~main.8fe7ee23.js"
+  },
+  {
+    "revision": "bc2cae1e9eebee9e0e65",
+    "url": "/vmp/static/js/main.bc2cae1e.chunk.js"
+  },
+  {
+    "revision": "e0502d90fefb61fa686c",
+    "url": "/vmp/static/js/1.e0502d90.chunk.js"
+  },
+  {
+    "revision": "bc2cae1e9eebee9e0e65",
+    "url": "/vmp/static/css/main.b20bb9c8.chunk.css"
+  },
+  {
+    "revision": "0635365dc2e4e4f65376808d3bad2bac",
+    "url": "/vmp/index.html"
+  }
+];
